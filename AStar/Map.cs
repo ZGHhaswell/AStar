1ff76922e2940c39431f78793eadb6b8f358f602
zhuguohao110 <zhuguohao110@163.com>
2015-03-01 18:00:43 +0800

    As-S--===

---
+++ AStar/Map.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar
+{
+    class Map
+    {
+        public const int MapSize = 10;
+
+        static int[,] MapData =  
+        { 
+            { 0,-1, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+            { 0,-1, 0, 0,-1,-1,-1,-1, 0, 0 }, 
+            { 0,-1, 1, 0,-1, 0, 2,-1, 0, 0 }, 
+            { 0,-1, 0, 0,-1, 0, 0, 0, 0, 0 }, 
+            { 0,-1, 0, 0,-1, 0, 0, 0, 0, 0 }, 
+            { 0,-1,-1,-1,-1, 0, 0, 0, 0, 0 }, 
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } 
+        };
+
+        public int GetMapData(int x, int y)
+        {
+            if (x < 0 || x > 9 || y < 0 || y > 9)
+                return -1;
+            else
+                return MapData[y, x];
+        }
+    }
+
+    class AStarNode2D : AStarNode
+    {
+        private int _x, _y;
+        private Map _map;
+
+        public AStarNode2D(int x, int y, Map map)
+        {
+            this._x = x;
+            this._y = y;
+            this._map = map;
+        }
+
+        public int X
+        {
+            get { return _x; }
+        }
+
+        public int Y
+        {
+            get { return _y; }
+        }
+
+        public override bool EqualTo(AStarNode node)
+        {
+            AStarNode2D n = (AStarNode2D)node;
+            return (n.X == this.X) && (n.Y == this.Y);
+        }
+
+        public override void Propagate(ArrayList successors)
+        {
+            AddSuccessor(_x, _y - 1, successors);
+            AddSuccessor(_x + 1, _y - 1, successors);
+            AddSuccessor(_x + 1, _y, successors);
+            AddSuccessor(_x + 1, _y + 1, successors);
+            AddSuccessor(_x, _y + 1, successors);
+            AddSuccessor(_x - 1, _y + 1, successors);
+            AddSuccessor(_x - 1, _y, successors);
+            AddSuccessor(_x - 1, _y - 1, successors);
+        }
+
+        private void AddSuccessor(int x, int y, ArrayList successors)
+        {
+            int cost = _map.GetMapData(x, y);
+            if (cost == -1) return;
+
+            AStarNode2D node = new AStarNode2D(x, y, _map);
+            AStarNode2D p = (AStarNode2D)this.Parent;
+            while (p != null)
+            {
+                if (node.EqualTo(p)) return;
+                p = (AStarNode2D)p.Parent;
+            }
+
+            successors.Add(node);
+        }
+        public override float GoalDistEstimate(AStarNode node)
+        {
+            return Cost(node);
+        }
+
+        public override float Cost(AStarNode node)
+        {
+            AStarNode2D n = (AStarNode2D)node;
+            int xd = n.X - this.X;
+            int yd = n.Y - this.Y;
+            return (float)Math.Sqrt(xd * xd + yd * yd);
+        }
+    } 
+}
---
+++ AStar/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map(); 
+            AStarNode2D start = new AStarNode2D(2, 2, map); 
+            AStarNode2D goal = new AStarNode2D(6, 2, map); 
+            AStarEngine engine = new AStarEngine(); 
+            ArrayList solution = new ArrayList(); 
+ 
+            if (engine.Execute(start, goal)) 
+            { 
+                AStarNode2D node = (AStarNode2D)engine.ResultNode; 
+                while (node != null) 
+                { 
+                    solution.Insert(0, node); 
+                    node = (AStarNode2D)node.Parent; 
+                } 
+ 
+                Console.WriteLine("Path found:"); 
+                for (int i = 0; i < 10; i ++) 
+                { 
+                    for (int j = 0; j < 10; j ++) 
+                    { 
+                        if (map.GetMapData(j, i) == -1) 
+                            Console.Write("X"); 
+                        else 
+                        { 
+                            AStarNode2D nt = new AStarNode2D(j, i, map); 
+                            bool inSolution = false; 
+ 
+                            foreach(AStarNode2D n in solution) 
+                            { 
+                                if (n.EqualTo(nt)) 
+                                { 
+                                    inSolution = true; 
+                                    break; 
+                                } 
+                            } 
+ 
+                            if (inSolution) Console.Write("S"); 
+                            else Console.Write("."); 
+                        } 
+                    } 
+ 
+                    Console.WriteLine(); 
+                } 
+            } 
+            else 
+            { 
+                Console.WriteLine("Unable to find a path."); 
+            } 
+ 
+            Console.ReadLine(); 
+        
+        }
+
+        
+    }
+}
